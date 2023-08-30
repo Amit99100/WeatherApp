a263b72cb0a4e89d52bdb6cad474223abd8c43bf
Amit Maurya <97939838+Amit99100@users.noreply.github.com>
2023-08-30 22:19:37 +0530

    Update 1.1 app.js

--- app.js
+++ app.js
@@ -14,34 +14,49 @@ const getWeather = async (city) => {
     weather.innerHTML = "<h2>Lodaing....↺</h2>";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     const response = await fetch(url)
-    // console.log(response);
+
     const data = await response.json();
     console.log(data);
     return showWeather(data);
 }
+
+
 
 const showWeather = (data) => {
 
 
     if (data.cod == '404') {
         weather.innerHTML = '<h2>City Not Found <h2>';
+        weather.style.color = "black";
         return;
-    }
-    weather.innerHTML = ` 
-    <img  src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"; >
-     <div>
-     <h2>${data.main.temp}°c</h2>
-     <h4>${data.weather[0].main}</h4>
-`;
+    } else {
 
-    weather1.innerHTML = `<h4>Feels_Like : ${data.main.feels_like}</h4>`
+        weather.innerHTML = ` 
+        <img style="color:"white" " src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"; >
+         <div>
+         
+         <h3>Weather</h3>
+         <h2>${data.main.temp}°C</h2>
+         <h4>${data.weather[0].description}</h4>
+    `;
+
+        document.getElementById("weather1").innerHTML = `<h4>Feels_Like : ${data.main.feels_like}</h4>`
+        document.getElementById("weather2").innerHTML = `<h4> Humidity: ${data.main.humidity}%</h4> <h4> Wind: ${Math.floor(data.wind.speed * 3.6)} km/h</h4>
+        <h4> deg: ${Math.floor(data.wind.deg)}°</h4>`
+        document.getElementById("weather3").innerHTML = ` <h4>Coordinate: </h4> 
+        <h4>Latitude:  ${Math.round(data.coord.lat * 100) / 100} </h4>
+        <h4>Longitude:  ${Math.round(data.coord.lon * 100) / 100}</h4>`
+        document.getElementById("weather4").innerHTML = `<h4>Country : ${data.sys.country} <img src="https://flagsapi.com/${data.sys.country}/shiny/32.png" ></h4>`
+    }
+
+
+
 
 }
 
 
 
 form.addEventListener("submit", (event) => {
-
     //as forms get submitted it start reloaidng 
     // that is default of forms 
     // you can try to submit it will start reloading 
@@ -51,6 +66,3 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
 }
 )
-
-
-
